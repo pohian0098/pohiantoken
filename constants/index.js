dd7492b63b744901673adde443149c24e9430ea4
pohian0098 <137751591+pohian0098@users.noreply.github.com>
2023-07-22 08:12:10 +0200

    Update index.js
    update contract address

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xda55d0eb54b3f918a5dce2bf704cf0f2d28f5acc";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
